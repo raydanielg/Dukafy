9427b9f8be3c32ad2359dd6040131bc60ba6a82a
raydanielg <airezra2+1@gmail.com>
2026-04-07 14:25:12 +0300

    autosave

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [App\Http\Controllers\Api\AuthController::class, 'register']);
+    Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/me', [App\Http\Controllers\Api\AuthController::class, 'me']);
+        Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout']);
+    });
+});
