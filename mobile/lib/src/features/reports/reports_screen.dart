130e3747ff0c038bc9868893cb84bf506f0b668f
raydanielg <airezra2+1@gmail.com>
2026-04-07 17:00:02 +0300

    autosave

---
+++ mobile/lib/src/features/reports/reports_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ReportsScreen extends StatelessWidget {
+  const ReportsScreen({super.key});
+
+  static const routeName = 'reports';
+  static const routePath = '/reports';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Business Reports', style: TextStyle(fontWeight: FontWeight.bold))),
+      body: const Center(child: Text('Reports Management Content')),
+    );
+  }
+}
