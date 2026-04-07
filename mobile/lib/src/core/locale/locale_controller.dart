a53453df6f71fe36138986016ed3cc41427143e3
raydanielg <airezra2+1@gmail.com>
2026-04-07 13:58:58 +0300

    autosave

--- mobile/lib/src/core/locale/locale_controller.dart
+++ mobile/lib/src/core/locale/locale_controller.dart
@@ -3,26 +3,25 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../storage/storage_providers.dart';
 
-class LocaleController extends StateNotifier<Locale> {
-  LocaleController(this._ref) : super(const Locale('en')) {
-    _load();
+class LocaleController extends Notifier<Locale> {
+  @override
+  Locale build() {
+    Future.microtask(_load);
+    return const Locale('en');
   }
 
-  final Ref _ref;
-
   Future<void> _load() async {
-    final code = await _ref.read(secureStorageProvider).getLocaleCode();
+    final code = await ref.read(secureStorageProvider).getLocaleCode();
     if (code == null || code.isEmpty) return;
     state = Locale(code);
   }
 
   Future<void> setLocale(Locale locale) async {
     state = locale;
-    await _ref.read(secureStorageProvider).setLocaleCode(locale.languageCode);
+    await ref.read(secureStorageProvider).setLocaleCode(locale.languageCode);
   }
 }
 
-final localeControllerProvider =
-    StateNotifierProvider<LocaleController, Locale>((ref) {
-  return LocaleController(ref);
-});
+final localeControllerProvider = NotifierProvider<LocaleController, Locale>(
+  LocaleController.new,
+);
